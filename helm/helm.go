081b8636f3fbbdb6f56468d13997cbebe1c8d1bc
GeekTR <geektheripper@gmail.com>
2025-03-06 23:53:18 +0800

    fix helm type

--- helm/helm.go
+++ helm/helm.go
@@ -4,23 +4,9 @@ import "time"
 
 // https://helm.sh/docs/topics/charts/
 type Repo struct {
-	ApiVersion   string             `yaml:"apiVersion"`
-	Name         string             `yaml:"name"`
-	Version      string             `yaml:"version"`
-	KubeVersion  string             `yaml:"kubeVersion"`
-	Description  string             `yaml:"description"`
-	Type         string             `yaml:"type"`
-	Keywords     []string           `yaml:"keywords"`
-	Home         string             `yaml:"home"`
-	Sources      []string           `yaml:"sources"`
-	Dependencies []Dependency       `yaml:"dependencies"`
-	Maintainers  []Maintainer       `yaml:"maintainers"`
-	Icon         string             `yaml:"icon"`
-	AppVersion   string             `yaml:"appVersion"`
-	Deprecated   bool               `yaml:"deprecated"`
-	Annotations  map[string]string  `yaml:"annotations"`
-	EntriesMap   map[string][]Chart `yaml:"entries"`
-	Generated    time.Time          `yaml:"generated"`
+	ApiVersion string             `yaml:"apiVersion"`
+	EntriesMap map[string][]Chart `yaml:"entries"`
+	Generated  time.Time          `yaml:"generated"`
 }
 
 type Maintainer struct {
